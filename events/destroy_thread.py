242ab2e9f73e40eb3a648d26f3b06b1b71776713
Dillon Barnes <dillonb07dev@gmail.com>
2024-10-31 17:29:27 +0000

    add thread logs

--- events/destroy_thread.py
+++ events/destroy_thread.py
@@ -7,12 +7,24 @@ async def destroy_thread(user_id: str, channel_id: str, root_ts: str, client: As
     messages = await client.conversations_replies(
         channel=channel_id, ts=root_ts
     )
+    message_log = []
     for message in messages["messages"]:
+        user_id = message.get('user', 'Unknown user')
+        message_log.append(f"{user_id}: {message.get('text', 'There was no text associated with this message')}")
         add_message_to_delete_queue(
             channel_id=channel_id, message_ts=message["ts"]
         )
-    # send first message to logs
-    await client.chat_postMessage(
+    
+    message_log = "\n".join(message_log)
+
+    log_msg = await client.chat_postMessage(
         channel=env.slack_log_channel,
-        text=f"Thread `{root_ts}` is being destroyed in <#{channel_id}> by <@{user_id}>.\nFirst message:\n```{messages['messages'][0]['text']}```",
+        text=f"Thread `{root_ts}` has been destroyed in <#{channel_id}> by <@{user_id}>.\nFirst message:\n```{messages['messages'][0]['text']}```",
+    )
+
+    await client.files_upload_v2(
+        channel=env.slack_log_channel,
+        thread_ts=log_msg["ts"],
+        content=message_log,
+        initial_comment="Message log for the destroyed thread"
     )
